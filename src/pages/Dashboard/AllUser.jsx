e07b9769b5fa02b8a2704e3099cb3a6809ac0b5b
webdev-mostofa <mdmostofaa24@gmail.com>
2024-07-27 15:18:48 +0600

    search by user name implement done

--- src/pages/Dashboard/AllUser.jsx
+++ src/pages/Dashboard/AllUser.jsx
@@ -6,6 +6,7 @@ import { LuShieldClose } from "react-icons/lu";
 import toast from "react-hot-toast";
 import { FiSearch } from "react-icons/fi";
 import { useState } from "react";
+import noresults from "../../img/no-results.png";
 
 const AllUser = () => {
   const axiosPublic = useAxiosPublic();
@@ -45,7 +46,6 @@ const AllUser = () => {
     setName(name);
   };
 
-  console.log(name);
   if (isLoading) return <Spinner />;
   return (
     <section>
@@ -77,124 +77,140 @@ const AllUser = () => {
             </form>
           </div>
         </div>
-
-        <div className="flex flex-col mt-6">
-          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden border border-gray-200  md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
-                      >
-                        <div className="flex items-center gap-x-3">
-                          <span>Name</span>
-                        </div>
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
-                      >
-                        <div className="flex items-center gap-x-3">
-                          <span>Email</span>
-                        </div>
-                      </th>
-
-                      <th
-                        scope="col"
-                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-                      >
-                        <span>Phone</span>
-                      </th>
-
-                      <th
-                        scope="col"
-                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-                      >
-                        Status
-                      </th>
-
-                      <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200 ">
-                    {users?.map((user) => (
-                      <tr key={user?._id}>
-                        <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                          {user?.name}
-                        </td>
-                        <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                          {user?.email}
-                        </td>
-
-                        <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                          {user?.mobile}
-                        </td>
-
-                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                          <div
-                            className={`inline-flex items-center px-2 py-1 rounded-md gap-x-1 ${
-                              user?.status === "pending"
-                                ? "bg-yellow-100/60 text-yellow-500"
-                                : user?.status === "activate"
-                                ? "bg-green-100/60 text-green-500"
-                                : "bg-red-100/60 text-red-500"
-                            } `}
+        {users?.length === 0 ? (
+          <div className="flex h-[90vh] justify-center items-center">
+            <img className="w-1/2 lg:w-1/3" src={noresults} alt="" />
+          </div>
+        ) : (
+          <>
+            <div className="flex flex-col mt-6">
+              <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                  <div className="overflow-hidden border border-gray-200  md:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead className="bg-gray-50">
+                        <tr>
+                          <th
+                            scope="col"
+                            className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
                           >
-                            {user?.status === "pending" && (
-                              <span>
-                                <LuShieldClose size={18} />
-                              </span>
-                            )}
-                            {user?.status === "activate" && (
-                              <span>
-                                <MdVerifiedUser size={18} />
-                              </span>
-                            )}
-                            {user?.status === "block" && (
-                              <span>
-                                <MdBlockFlipped size={18} />
-                              </span>
-                            )}
-                            <h2 className="text-sm font-normal capitalize">
-                              {" "}
-                              {user?.status}
-                            </h2>
-                          </div>
-                        </td>
-                        <td className="px-4 py-4 text-sm whitespace-nowrap">
-                          <div className="flex items-center gap-x-6">
-                            <button
-                              disabled={
-                                user?.status === "activate" ||
-                                user?.status === "block"
-                              }
-                              onClick={() => handleActivate(user)}
-                              className="disabled:cursor-not-allowed flex items-center px-3 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-                            >
-                              <span className="mx-1">Activate</span>
-                            </button>
-                            <button
-                              disabled={user?.status === "block"}
-                              onClick={() => handleBlock(user)}
-                              className="disabled:cursor-not-allowed flex items-center px-3 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-                            >
-                              <span className="mx-1">Block</span>
-                            </button>
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
+                            <div className="flex items-center gap-x-3">
+                              <span>Name</span>
+                            </div>
+                          </th>
+                          <th
+                            scope="col"
+                            className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
+                          >
+                            <div className="flex items-center gap-x-3">
+                              <span>Email</span>
+                            </div>
+                          </th>
+
+                          <th
+                            scope="col"
+                            className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                          >
+                            <span>Phone</span>
+                          </th>
+
+                          <th
+                            scope="col"
+                            className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                          >
+                            Status
+                          </th>
+
+                          <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                            Actions
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="bg-white divide-y divide-gray-200 ">
+                        {users?.map((user) => (
+                          <tr key={user?._id}>
+                            <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                              {user?.name}
+                            </td>
+                            <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                              {user?.email}
+                            </td>
+
+                            <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                              {user?.mobile}
+                            </td>
+
+                            <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                              <div
+                                className={`inline-flex items-center px-2 py-1 rounded-md gap-x-1 ${
+                                  user?.status === "pending"
+                                    ? "bg-yellow-100/60 text-yellow-500"
+                                    : user?.status === "activate"
+                                    ? "bg-green-100/60 text-green-500"
+                                    : "bg-red-100/60 text-red-500"
+                                } `}
+                              >
+                                {user?.status === "pending" && (
+                                  <span>
+                                    <LuShieldClose size={18} />
+                                  </span>
+                                )}
+                                {user?.status === "activate" && (
+                                  <span>
+                                    <MdVerifiedUser size={18} />
+                                  </span>
+                                )}
+                                {user?.status === "block" && (
+                                  <span>
+                                    <MdBlockFlipped size={18} />
+                                  </span>
+                                )}
+                                <h2 className="text-sm font-normal capitalize">
+                                  {" "}
+                                  {user?.status}
+                                </h2>
+                              </div>
+                            </td>
+                            <td className="px-4 py-4 text-sm whitespace-nowrap">
+                              <div className="flex items-center gap-x-6">
+                                <button
+                                  disabled={
+                                    user?.status === "activate" ||
+                                    user?.status === "block"
+                                  }
+                                  onClick={() => handleActivate(user)}
+                                  className={`flex items-center px-3 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform focus:outline-none focus:ring rounded-lg focus:ring-blue-300 focus:ring-opacity-80 ${
+                                    user?.status === "activate" ||
+                                    user?.status === "block"
+                                      ? "bg-gray-400 cursor-not-allowed"
+                                      : "bg-blue-600 hover:bg-blue-500"
+                                  }`}
+                                >
+                                  <span className="mx-1">Activate</span>
+                                </button>
+                                <button
+                                  disabled={user?.status === "block"}
+                                  onClick={() => handleBlock(user)}
+                                  className={`flex items-center px-3 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform focus:outline-none focus:ring rounded-lg focus:ring-blue-300 focus:ring-opacity-80 ${
+                                    user?.status === "block"
+                                      ? "bg-gray-400 cursor-not-allowed"
+                                      : "bg-blue-600 hover:bg-blue-500"
+                                  }`}
+                                >
+                                  <span className="mx-1">Block</span>
+                                </button>
+                              </div>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </section>
   );
